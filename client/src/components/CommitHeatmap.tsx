0a00cc04bea1a079ea4313c06780ba93de255b1c
Sivasaikrishna19 <sivasaikrishna.nekkalapu@sjsu.edu>
2024-07-12 15:58:47 -0700

    year wise commit history filtering added

--- client/src/components/CommitHeatmap.tsx
+++ client/src/components/CommitHeatmap.tsx
@@ -6,77 +6,124 @@ import { useSelector } from "react-redux";
 import { UserState } from "@/store/slices/profileSummary";
 import Cookies from "js-cookie";
 import { fetchUserCommitHistory } from "@/graphql/queries/commitHistory";
+import { Select, Tooltip } from "antd";
+
+const { Option } = Select;
 
 const CommitHeatmap = () => {
   const [commitActivity, setCommitActivity] = useState<
     { date: string; count: number }[]
   >([]);
+  const [availableYears, setAvailableYears] = useState<number[]>([]);
+  const [selectedYear, setSelectedYear] = useState<number>(
+    new Date().getFullYear()
+  );
+
   const { profileSummary }: any = useSelector(
     (state: UserState) => state.profileSummary
   );
   const accessToken: string | undefined = Cookies.get("access_token");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const commitHistory = await fetchUserCommitHistory(
-          profileSummary.login,
-          accessToken!
-        );
-        const commitActivities = commitHistory.user.repositories.edges.flatMap(
-          (repo: any) => {
-            if (
-              repo.node.defaultBranchRef &&
-              repo.node.defaultBranchRef.target &&
-              repo.node.defaultBranchRef.target.history
-            ) {
-              return repo.node.defaultBranchRef.target.history.edges.map(
-                (commit: any) => ({
-                  date: commit.node.committedDate.split("T")[0],
-                  count: 1,
-                })
-              );
-            }
-            return [];
+  const fetchData = async (year: number) => {
+    try {
+      const commitHistory = await fetchUserCommitHistory(
+        profileSummary.login,
+        accessToken!,
+        year
+      );
+      const commitActivities = commitHistory.user.repositories.edges.flatMap(
+        (repo: any) => {
+          if (
+            repo.node.defaultBranchRef &&
+            repo.node.defaultBranchRef.target &&
+            repo.node.defaultBranchRef.target.history
+          ) {
+            return repo.node.defaultBranchRef.target.history.edges.map(
+              (commit: any) => ({
+                date: commit.node.committedDate.split("T")[0],
+                count: 1,
+              })
+            );
           }
-        );
+          return [];
+        }
+      );
 
-        const aggregatedData = commitActivities.reduce(
-          (acc: { [date: string]: number }, { date, count }: any) => {
-            if (date) {
-              acc[date] = (acc[date] || 0) + count;
-            }
-            return acc;
-          },
-          {}
-        );
+      const aggregatedData = commitActivities.reduce(
+        (acc: { [date: string]: number }, { date, count }: any) => {
+          if (date) {
+            acc[date] = (acc[date] || 0) + count;
+          }
+          return acc;
+        },
+        {}
+      );
 
-        const heatmapData: any = Object.entries(aggregatedData).map(
-          ([date, count]) => ({
-            date,
-            count,
-          })
-        );
+      const heatmapData: any = Object.entries(aggregatedData).map(
+        ([date, count]) => ({
+          date,
+          count,
+        })
+      );
 
-        setCommitActivity(heatmapData);
-      } catch (error) {
-        console.error("Error fetching commit activities:", error);
+      setCommitActivity(heatmapData);
+    } catch (error) {
+      console.error("Error fetching commit activities:", error);
+    }
+  };
+
+  const handleYearChange = (year: number) => {
+    setSelectedYear(year);
+    fetchData(year);
+  };
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      if (profileSummary.login) {
+        try {
+          // Fetch the user creation year
+          const userCreationYear = new Date(
+            profileSummary.createdAt
+          ).getFullYear();
+          const currentYear = new Date().getFullYear();
+          const years = [];
+          for (let year = userCreationYear; year <= currentYear; year++) {
+            years.push(year);
+          }
+          setAvailableYears(years);
+
+          fetchData(selectedYear);
+        } catch (error) {
+          console.error("Error fetching initial data:", error);
+        }
       }
     };
 
-    if (profileSummary.login && accessToken) {
-      fetchData();
-    }
-  }, [profileSummary.login, accessToken]);
+    fetchInitialData();
+  }, [profileSummary.login, profileSummary.createdAt]);
 
   return (
-    <div className="bg-[#d4e7fa] p-4 rounded-md shadow-mg mt-4 overflow-y-auto">
-      <div className="text-semibold text-[20px] text-center mb-2">
+    <div className="bg-[#d4e7fa] p-6 rounded-md shadow-lg mt-6">
+      <div className="text-semibold text-2xl text-center mb-4">
         Commit History
       </div>
+      <div className="flex justify-center mb-4">
+        <Select
+          defaultValue={selectedYear}
+          style={{ width: 200 }}
+          onChange={handleYearChange}
+          className="w-full max-w-xs"
+        >
+          {availableYears.map((year) => (
+            <Option key={year} value={year}>
+              {year}
+            </Option>
+          ))}
+        </Select>
+      </div>
       <CalendarHeatmap
-        startDate={new Date("2023-07-01")}
-        endDate={new Date("2024-07-31")}
+        startDate={new Date(`${selectedYear}-01-01`)}
+        endDate={new Date(`${selectedYear}-12-31`)}
         values={commitActivity}
         classForValue={(value) => {
           if (!value) {
@@ -86,10 +133,24 @@ const CommitHeatmap = () => {
         }}
         tooltipDataAttrs={(value: any) => {
           return {
-            "data-tip": `${value.date}: ${value.count} commits`,
+            "data-tip": `${value.date}: ${value.count} commit${
+              value.count > 1 ? "s" : ""
+            }`,
           };
         }}
       />
+      {/* {commitActivity.map((activity) => (
+        <Tooltip
+          key={activity.date}
+          title={`${activity.date}: ${activity.count} commit${
+            activity.count > 1 ? "s" : ""
+          }`}
+        >
+          <span className={`color-github-${Math.min(activity.count, 4)}`}>
+            {activity.date}
+          </span>
+        </Tooltip>
+      ))} */}
     </div>
   );
 };
--- client/src/graphql/queries/commitHistory.ts
+++ client/src/graphql/queries/commitHistory.ts
@@ -1,10 +1,12 @@
 // src/graphql/queries/repository.ts
 import { graphqlClient } from '../client';
 
-export const fetchUserCommitHistory = async (username: string, token: string) => {
+export const fetchUserCommitHistory = async (username: string, token: string, year: number) => {
+  const startDate = new Date(`${year}-01-01`).toISOString();
+  const endDate = new Date(`${year}-12-31`).toISOString();
 
   const query = `
-    query($username: String!, $first: Int!, $after: String) {
+    query($username: String!, $first: Int!, $after: String, $startDate: GitTimestamp!, $endDate: GitTimestamp!) {
       user(login: $username) {
         repositories(first: $first, after: $after) {
           edges {
@@ -13,7 +15,7 @@ export const fetchUserCommitHistory = async (username: string, token: string) =>
               defaultBranchRef {
                 target {
                   ... on Commit {
-                    history(first: 100) {
+                    history(first: 100, since: $startDate, until: $endDate) {
                       edges {
                         node {
                           committedDate
@@ -34,6 +36,8 @@ export const fetchUserCommitHistory = async (username: string, token: string) =>
     username,
     first: 100,
     after: null,
+    startDate,
+    endDate,
   };
 
   return graphqlClient(query, variables, token);
