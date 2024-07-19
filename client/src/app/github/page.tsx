8b029f98cc1812729c5d762e7cd0295818a18b71
Sivasaikrishna19 <sivasaikrishna.nekkalapu@sjsu.edu>
2024-07-18 21:15:13 -0700

    header updated

--- client/src/app/github/page.tsx
+++ client/src/app/github/page.tsx
@@ -113,10 +113,16 @@ const Page = () => {
 
   return (
     <div>
-      <div className="flex items-center justify-center w-full text-[24px] mb-6 text-center">
-        Welcome to GitInsight! Discover top talent through GitHub profiles with
-        ease
+      <div className="flex flex-col items-center justify-center w-full text-center mb-6">
+        <div className="text-[32px] sm:text-[36px] md:text-[40px] lg:text-[44px] font-extrabold ">
+          Universe <span className="text-blue-500">GitInsight</span>
+        </div>
+        <div className="text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] mt-2 text-gray-700">
+          Discover top talent through GitHub profiles with ease
+        </div>
+        <div className="mt-4 w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 h-1 bg-blue-500 rounded-full"></div>
       </div>
+
       {token ? (
         <div className="w-full">
           <Search
