6364aee63ed9d7cb0b486b5e49ab1cd7a970e0bb
Sivasaikrishna19 <sivasaikrishna.nekkalapu@sjsu.edu>
2024-07-24 00:02:54 -0700

    prod next config updated

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,14 +3,16 @@ dotenv.config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:false,
-    env: {
-        // Ensure that these environment variables match those in your .env.local file
-        NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
-        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-        NEXT_PUBLIC_DEV_URL: process.env.NEXT_PUBLIC_DEV_URL,
-        NEXT_PUBLIC_PROD_URL: process.env.NEXT_PUBLIC_PROD_URL,
-      },
+  reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    NEXT_PUBLIC_DEV_URL: process.env.NEXT_PUBLIC_DEV_URL,
+    NEXT_PUBLIC_PROD_URL: process.env.NEXT_PUBLIC_PROD_URL,
+    NEXT_PUBLIC_LINKEDIN_CLIENT_SECRET_PROD: process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_SECRET_PROD,
+    NEXT_PUBLIC_LINKEDIN_CLIENT_ID_PROD: process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID_PROD,
+    NEXT_PUBLIC_LINKEDIN_REDIRECT_URI_PROD: process.env.NEXT_PUBLIC_LINKEDIN_REDIRECT_URI_PROD,
+  },
 };
 
 export default nextConfig;
